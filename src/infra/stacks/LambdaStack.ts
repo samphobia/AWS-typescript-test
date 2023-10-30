07f6eb3c250ff2073cb5c4a92690b996a76ac877
Samuel Osinubi <samobafemisayo@gmail.com>
2023-10-30 15:09:08 +0100

    update

--- src/infra/stacks/LambdaStack.ts
+++ src/infra/stacks/LambdaStack.ts
@@ -1,8 +1,10 @@
 import { Stack, StackProps } from 'aws-cdk-lib'
 import { LambdaIntegration } from 'aws-cdk-lib/aws-apigateway'
 import { ITable } from 'aws-cdk-lib/aws-dynamodb'
+import { Effect, PolicyStatement } from 'aws-cdk-lib/aws-iam'
 import { Function as LambdaFunction, Runtime } from 'aws-cdk-lib/aws-lambda'
 import { Code } from 'aws-cdk-lib/aws-lambda'
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs'
 import { Construct } from 'constructs'
 import { join } from 'path'
 
@@ -17,14 +19,23 @@ export class LambdaStack extends Stack {
   constructor(scope: Construct, id: string, props: LambdaStackProps) {
     super(scope, id, props)
 
-    const helloLambda = new LambdaFunction(this, 'HelloLambda', {
+    const helloLambda = new NodejsFunction(this, 'HelloLambda', {
       runtime: Runtime.NODEJS_18_X,
-      handler: 'hello.main',
-      code: Code.fromAsset(join(__dirname, '..', '..', 'services')),
+      handler: 'handler',
+      entry: (join(__dirname, '..', '..', 'services', 'hello.ts')),
       environment: {
         TABLE_NAME: props.spacesTable.tableName
       }
     })
+
+    helloLambda.addToRolePolicy(new PolicyStatement({
+      effect: Effect.ALLOW,
+      actions: [
+        's3:ListAllMyBuckets',
+        's3:ListBucket'
+      ],
+      resources: ["*"]
+    }))
 
     this.helloLamndaIntegration = new LambdaIntegration(helloLambda)
   }
---
+++ src/services/hello.ts
@@ -0,0 +1,22 @@
+import { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from "aws-lambda";
+import { v4 } from 'uuid'
+import { S3Client, ListBucketsCommand } from "@aws-sdk/client-s3";
+
+const s3Client = new S3Client({})
+
+async function handler(event: APIGatewayProxyEvent, context: Context) {
+
+  const command = new ListBucketsCommand({})
+  const listBucketResult = (await (s3Client.send(command))).Buckets
+
+
+  const response: APIGatewayProxyResult = {
+    statusCode: 200,
+    body: JSON.stringify('Hello from lambda, here are your buckets:  this is the id:' + JSON.stringify(listBucketResult))
+  }
+  console.log(event)
+
+  return response
+}
+
+export { handler }
