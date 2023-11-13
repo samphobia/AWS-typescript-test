dbfd76f958dc3305735ec167a21eb5373843a804
Samuel Osinubi <samobafemisayo@gmail.com>
2023-11-13 14:46:33 +0100

    added user groups

--- src/infra/stacks/AuthStack.ts
+++ src/infra/stacks/AuthStack.ts
@@ -1,44 +1,52 @@
 import { CfnOutput, Stack, StackProps } from 'aws-cdk-lib'
-import { UserPool, UserPoolClient } from 'aws-cdk-lib/aws-cognito';
+import { CfnUserPoolGroup, UserPool, UserPoolClient } from 'aws-cdk-lib/aws-cognito';
 import { Construct } from 'constructs';
 
 
 export class AuthStack extends Stack {
 
-    public userPool: UserPool;
-    private userPoolClient: UserPoolClient;
+  public userPool: UserPool;
+  private userPoolClient: UserPoolClient;
 
-    constructor(scope: Construct, id: string, props?: StackProps) {
-        super(scope, id, props);
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
 
-        this.createUserPool();
-        this.createUserPoolClient();
-    }
+    this.createUserPool();
+    this.createUserPoolClient();
+    this.createAdminsGroup();
 
-    private createUserPool(){
-        this.userPool = new UserPool(this, 'SpaceUserPool', {
-            selfSignUpEnabled: true,
-            signInAliases: {
-                username: true,
-                email: true
-            }
-        });
+  }
 
-        new CfnOutput(this, 'SpaceUserPoolId', {
-            value: this.userPool.userPoolId
-        })
-    }
-    private createUserPoolClient(){
-        this.userPoolClient = this.userPool.addClient('SpaceUserPoolClient', {
-            authFlows: {
-                adminUserPassword: true,
-                custom: true,
-                userPassword: true,
-                userSrp: true
-            }
-        });
-        new CfnOutput(this, 'SpaceUserPoolClientId', {
-            value: this.userPoolClient.userPoolClientId
-        })
-    }
+  private createUserPool() {
+    this.userPool = new UserPool(this, 'SpaceUserPool', {
+      selfSignUpEnabled: true,
+      signInAliases: {
+        username: true,
+        email: true
+      }
+    });
+
+    new CfnOutput(this, 'SpaceUserPoolId', {
+      value: this.userPool.userPoolId
+    })
+  }
+  private createUserPoolClient() {
+    this.userPoolClient = this.userPool.addClient('SpaceUserPoolClient', {
+      authFlows: {
+        adminUserPassword: true,
+        custom: true,
+        userPassword: true,
+        userSrp: true
+      }
+    });
+    new CfnOutput(this, 'SpaceUserPoolClientId', {
+      value: this.userPoolClient.userPoolClientId
+    })
+  }
+  private createAdminsGroup(){
+    new CfnUserPoolGroup(this, 'SpaceAdmins', {
+        userPoolId: this.userPool.userPoolId,
+        groupName: 'admins'
+    })
+}
 }
--- src/services/shared/Utils.ts
+++ src/services/shared/Utils.ts
@@ -1,5 +1,6 @@
 import { randomUUID } from "crypto";
 import { JsonError } from "./Validator"
+import { APIGatewayProxyEvent } from "aws-lambda";
 
 export function createRandomId(){
   return randomUUID();
@@ -11,4 +12,12 @@ export function parseJSON(args: string) {
   } catch (error) {
     throw new JsonError(error.message)
   }
+}
+
+export function hasAdminGroup(event: APIGatewayProxyEvent) {
+  const groups = event.requestContext.authorizer?.claims['cognito:groups'];
+  if (groups) {
+    return (groups as string).includes('admins');
+  }
+  return false;
 }
